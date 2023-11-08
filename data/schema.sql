4fd4d1f9375cb96ebd5429445d8fda06dacada23
Andres Castro <softwarecorridor@users.noreply.github.com>
2023-11-07 19:14:28 -0500

    Update sql schema to handle potential entries

--- data/schema.sql
+++ data/schema.sql
@@ -11,3 +11,26 @@ CREATE TABLE IF NOT EXISTS transactions (
     FOREIGN KEY(description) REFERENCES descriptions(description_id)
 );
 
+CREATE TABLE IF NOT EXISTS potential_transaction (
+    date TEXT,
+    amount TEXT,
+    description INTEGER,
+    pass INTEGER
+);
+
+CREATE VIEW IF NOT EXISTS duplicate_transactions AS
+SELECT DISTINCT
+    pt.date AS potential_date,
+    pt.amount AS potential_amount,
+    pt.description AS potential_description,
+    t.date AS actual_date,
+    t.amount AS actual_amount,
+    t.description AS actual_description
+FROM
+    transactions T
+INNER JOIN
+    potential_transaction pt
+ON
+    potential_date = actual_date
+	AND potential_amount = actual_amount
+    AND potential_description = actual_description
