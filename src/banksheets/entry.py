a764040ae48ed0f38400be5389d307450763aa46
Andres Castro <softwarecorridor@users.noreply.github.com>
2024-01-07 14:42:10 -0500

    Use sql headers

--- src/banksheets/entry.py
+++ src/banksheets/entry.py
@@ -1,17 +1,22 @@
-from datetime import datetime
 from dataclasses import dataclass
+from datetime import datetime
 
-@dataclass(order=True, frozen = True)
+
+@dataclass(order=True, frozen=True)
 class DataEntry:
-        date: datetime
-        data_id: int
-        description: str
-        extra_description:str
-        amount: float
+    date: datetime
+    amount: float
+    description: str
+    extra_desc: str = ""
 
-        def __post_init__(self):
-            object.__setattr__(self, 'date', datetime.strptime(self.date, "%m/%d/%Y"))
+    def __post_init__(self):
+        object.__setattr__(self, "date", datetime.strptime(self.date, "%m/%d/%Y"))
+        object.__setattr__(self, "amount", float(self.amount))
 
-        def __str__(self) -> str:
-            return f"{self.date.strftime('%m/%d/%Y')}\t{self.amount}\t{self.description}\t{self.extra_description}"
-
+    def __str__(self) -> str:
+        return (
+            f"{self.date.strftime('%m/%d/%Y')}\t"
+            f"{self.amount}\t"
+            f"{self.description}\t"
+            f"{self.extra_desc}"
+        )
--- tests/test_entry.py
+++ tests/test_entry.py
@@ -1,126 +1,93 @@
+from pytest import fixture, raises
+
 from banksheets.entry import DataEntry
-from pytest import raises
 
 
-def test_parse():
-    data = [
-        "02/11/2021",
-        "491091136970529302963377",
-        "Popeyes",
-        "Frankfurt        IL ",
-        "-10.02",
-    ]
-    under_test = DataEntry(*data)
-    assert under_test.date.strftime("%m/%d/%Y") == data[0]
-    assert under_test.amount == data[4]
-    assert under_test.description == data[2]
+@fixture
+def transaction_a():
+    return {
+        "date": "01/01/2023",
+        "description": "Company A",
+        "extra_desc": "City1, Street A",
+        "amount": "100.25",
+    }
+
+
+@fixture
+def transaction_a_negative():
+    return {
+        "date": "01/01/2023",
+        "description": "Company A",
+        "extra_desc": "City1, Street A",
+        "amount": "-100.25",
+    }
+
+
+@fixture
+def transaction_b():
+    return {
+        "date": "02/15/2023",
+        "description": "Company B",
+        "amount": "-150.75",
+    }
+
+
+@fixture
+def transaction_c():
+    return {
+        "date": "03/10/2023",
+        "description": "Company C",
+        "extra_desc": "City3, Street C",
+        "amount": "200",
+    }
+
+
+def test_parse(transaction_a):
+    under_test = DataEntry(**transaction_a)
+    assert under_test.date.strftime("%m/%d/%Y") == transaction_a["date"]
+    assert under_test.amount == float(transaction_a["amount"])
+    assert under_test.description == transaction_a["description"]
+    assert under_test.extra_desc == transaction_a["extra_desc"]
 
 
 def test_fail_parse():
-    data = ["Posted Date", "Reference Number", "Payee", "Address", "Amount"]
+    data = ["2020-01-01", "company a", "exrta b", 1]
     with raises(ValueError):
         DataEntry(*data)
 
 
-def test_order_simple():
-    data_1 = [
-        "02/11/2021",
-        "772566432424956988368115",
-        "RALLY",
-        "Frankfurt        IL ",
-        "-10.02",
-    ]
-    data_2 = [
-        "04/11/2021",
-        "013389515160328025198240",
-        "RALLY",
-        "Frankfurt        IL ",
-        "-10.02",
-    ]
-    data_3 = [
-        "03/11/2021",
-        "970623919179822408572244",
-        "RALLY",
-        "Frankfurt        IL ",
-        "-10.02",
-    ]
-
-    ut_1 = DataEntry(*data_1)
-    ut_2 = DataEntry(*data_2)
-    ut_3 = DataEntry(*data_3)
+def test_order_simple(transaction_a, transaction_b, transaction_c):
+    ut_1 = DataEntry(**transaction_a)
+    ut_2 = DataEntry(**transaction_c)
+    ut_3 = DataEntry(**transaction_b)
     under_test = [ut_1, ut_2, ut_3]
     under_test.sort()
     assert under_test == [ut_1, ut_3, ut_2]
 
 
-def test_equality():
-    data_1 = [
-        "01/04/2021",
-        "491298688675177918190816",
-        "RALLY",
-        "PARIS       WA ",
-        "-13.59",
-    ]
-    data_2 = [
-        "01/04/2021",
-        "728955390304896999654613",
-        "RALLY",
-        "PARIS       WA ",
-        "13.59",
-    ]
-    data_3 = [
-        "01/02/2021",
-        "240042477354516622049970",
-        "RALLY",
-        "PARIS       WA ",
-        "-13.59",
-    ]
-
-    ut_1 = DataEntry(*data_1)
-    ut_2 = DataEntry(*data_2)
-    ut_3 = DataEntry(*data_3)
-    assert ut_1 != ut_2
-    assert ut_1 != ut_3
-    assert ut_2 != ut_3
+def test_equality(transaction_a):
+    ut_1 = DataEntry(**transaction_a)
+    ut_2 = DataEntry(**transaction_a)
+    assert ut_1 == ut_2
 
 
-def test_inequality():
-    data_1 = [
-        "02/11/2021",
-        "740387467554245504019699",
-        "RALLY",
-        "SEATTLE        NY ",
-        "-10.02",
-    ]
-    data_2 = [
-        "03/11/2021",
-        "559212285141588665464875",
-        "RALLY",
-        "SEATTLE        NY ",
-        "-10.02",
-    ]
-
-    ut_1 = DataEntry(*data_1)
-    ut_2 = DataEntry(*data_2)
+def test_inequality(transaction_a, transaction_a_negative):
+    ut_1 = DataEntry(**transaction_a)
+    ut_2 = DataEntry(**transaction_a_negative)
     assert ut_1 != ut_2
 
 
-def test_lt():
-    data_1 = [
-        "02/11/2021",
-        "640385912017924538143779",
-        "RALLY",
-        "SEATTLE        NY ",
-        "-10.02",
-    ]
-    data_2 = [
-        "03/11/2021",
-        "077418787182427846248758",
-        "RALLY",
-        "SEATTLE        NY ",
-        "-10.02",
-    ]
-
-    ut_1 = DataEntry(*data_1)
-    ut_2 = DataEntry(*data_2)
+def test_lt(transaction_a, transaction_b):
+    ut_1 = DataEntry(**transaction_a)
+    ut_2 = DataEntry(**transaction_b)
     assert ut_1 < ut_2
+
+
+def test_to_str(transaction_a):
+    ut_1 = DataEntry(**transaction_a)
+    date_str = transaction_a["date"]
+    amount_str = transaction_a["amount"]
+    description_str = transaction_a["description"]
+    extra_desc_str = transaction_a["extra_desc"]
+    expected = f"{date_str}\t{amount_str}\t{description_str}\t{extra_desc_str}"
+    assert str(ut_1) == expected
