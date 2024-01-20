03f0d70e391361391f5d0cc10d213e48ff687a95
Andres Castro <softwarecorridor@users.noreply.github.com>
2024-01-20 17:01:01 -0500

    Add insert_description tests

--- tests/test_sql.py
+++ tests/test_sql.py
@@ -1,13 +1,19 @@
 from sqlite3 import Connection
 
-from pytest import fixture
+from pytest import fixture, raises
 
-from banksheets.database import create_sql_connection
+from banksheets.database import create_sql_connection, insert_descriptions
+from banksheets.entry import DataEntry
 
 
 @fixture
 def sql() -> Connection:
     return create_sql_connection(":memory:")
+
+
+@fixture
+def generic_entry() -> DataEntry:
+    return DataEntry("01/01/2023", "100.25", "Company A", "City1, Street A")
 
 
 def test_create(sql: Connection):
@@ -19,3 +25,27 @@ def test_create(sql: Connection):
         assert "description" in res
         assert "bank_transaction" in res
         assert "potential_transaction" in res
+
+
+def test_description_none(sql: Connection):
+    with raises(TypeError):
+        with sql as conn:
+            insert_descriptions(None, conn)
+
+
+def test_description_single(sql: Connection, generic_entry: DataEntry):
+    with sql as conn:
+        insert_descriptions([generic_entry], conn)
+        description = "SELECT name FROM description;"
+        c = conn.execute(description)
+        res = c.fetchone()[0]
+        assert generic_entry.description == res
+
+
+def test_description_empty(sql: Connection):
+    with sql as conn:
+        insert_descriptions([], conn)
+        description = "SELECT name FROM description;"
+        c = conn.execute(description)
+        res = c.fetchone()
+        assert res is None
