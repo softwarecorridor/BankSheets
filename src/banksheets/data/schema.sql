5bc94a92f289b73ed653a676c78c53c3438f859a
Andres Castro <softwarecorridor@users.noreply.github.com>
2024-03-03 14:30:29 -0500

    Reduce clutter in duplicate_view
    Just return a list of potential entries that are also in
    bank_transaction.

--- src/banksheets/data/schema.sql
+++ src/banksheets/data/schema.sql
@@ -6,6 +6,7 @@ CREATE TABLE IF NOT EXISTS description (
 );
 
 CREATE TABLE IF NOT EXISTS bank_transaction (
+    id INTEGER PRIMARY KEY,
     date TEXT,
     amount TEXT,
     description INTEGER,
@@ -13,19 +14,15 @@ CREATE TABLE IF NOT EXISTS bank_transaction (
 );
 
 CREATE TABLE IF NOT EXISTS potential_transaction (
+    id INTEGER PRIMARY KEY,
     date TEXT,
     amount TEXT,
     description INTEGER,
     FOREIGN KEY(description) REFERENCES description(description_id)
 );
 
+-- TODO: return just a potential transaction id?
 CREATE VIEW IF NOT EXISTS duplicate_view AS
-    SELECT
-        pt.date,
-        pt.amount,
-        d.name,
-        CASE WHEN COUNT(bt.date) > 1 THEN 1 ELSE 0 END AS duplicate_bank,
-        CASE WHEN COUNT(pt.date) > 1 THEN 1 ELSE 0 END AS duplicate_potential
-    FROM potential_transaction as pt
-    LEFT JOIN bank_transaction bt ON bt.date = pt.date AND bt.amount = pt.amount AND bt.description = pt.description
-	INNER JOIN description d ON d.description_id = pt.description;
+    SELECT pt.date, pt.amount, d.name FROM potential_transaction as pt
+    INNER JOIN bank_transaction bt ON bt.date = pt.date AND bt.amount = pt.amount AND bt.description = pt.description
+    INNER JOIN description d ON d.description_id = pt.description;
